d87a5074ad5720ba7809d96e445f6306641e0f41
FR4NC <francisedgard16@gmail.com>
2024-11-22 00:17:40 +0800

    update ule

--- src/pages/Verificationpage.jsx
+++ src/pages/Verificationpage.jsx
@@ -1,6 +1,10 @@
 import Verify from '../layout/verifyLayout'
+import { useEffect } from 'react'
 
 function Verificationpage() {
+    useEffect(() => {
+        document.title = "Verification Signup"
+    })
   return (
     <>
       <Verify />
